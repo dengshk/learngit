e5d4b37381755aee0ac1f851991aa94db93baba2
hadoop <hadoop>
2016-02-05 11:27:23 +0800

    wrote a mysql file

---
+++ mysql.sql
@@ -0,0 +1,15 @@
+-- 潼南县,永川区,垫江县
+drop table grid_rt_road_50x;
+rename table grid_rt_road_50x1 to grid_rt_road_50x;
+
+-- 綦江,梁平
+drop table grid_rt_road_50x;
+rename table grid_rt_road_50x2 to grid_rt_road_50x;
+
+-- 合川,忠县
+drop table grid_rt_road_50x;
+rename table grid_rt_road_50x3 to grid_rt_road_50x;
+
+-- 渝北,巴南,九龙坡
+drop table grid_rt_road_50x;
+rename table grid_rt_road_50x4 to grid_rt_road_50x;
